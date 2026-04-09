3ed8262b6395a05418ca110bdd72235afd089465
Ruan-Haack <ruanhaack06@gmail.com>
2026-04-09 20:55:17 -0300

    [feat] Adição da lógica de conversão para decimal em stack-binary.cpp

--- ed1/stack-binary.cpp
+++ ed1/stack-binary.cpp
@@ -1,25 +1,26 @@
-#include <iostream> 
-
-class Nodo 
+#include <iostream>
+#include <math.h>
+class Nodo
 {
-    public: 
-        int info;
-        Nodo *prox;
+public:
+    int info;
+    Nodo *prox;
 };
 
-class Pilha 
+class Pilha
 {
-    private: 
-        Nodo *topo;
-    public:
+private:
+    Nodo *topo;
+
+public:
     Pilha();
-    void empilhar(int num);
+    void empilhar(int n);
     int desempilhar();
+    int ConverDec();
     void imprimir();
-
 };
 
-Pilha::Pilha() 
+Pilha::Pilha()
 {
     topo = nullptr;
 };
@@ -28,14 +29,15 @@ void Pilha::empilhar(int n)
 {
     Nodo *novo;
     novo = new Nodo;
-    if(novo == nullptr) exit(1);
+    if (novo == nullptr)
+        exit(1);
 
     novo->info = n;
     novo->prox = nullptr;
 
-    if(topo != nullptr)
+    if (topo != nullptr)
         novo->prox = topo;
-    
+
     topo = novo;
 };
 
@@ -43,11 +45,12 @@ int Pilha::desempilhar()
 {
     int deletado;
     Nodo *aux;
-    if(topo == nullptr)
+    if (topo == nullptr)
     {
         std::cout << "Pilha vazia...";
         exit(0);
-    } else 
+    }
+    else
     {
         aux = topo;
         deletado = aux->info;
@@ -55,14 +58,13 @@ int Pilha::desempilhar()
         delete aux;
         return deletado;
     }
-
 };
 
 void Pilha::imprimir()
 {
     Nodo *aux;
     aux = topo;
-    while(aux != nullptr)
+    while (aux != nullptr)
     {
         std::cout << aux->info;
         aux = aux->prox;
@@ -72,31 +74,57 @@ void Pilha::imprimir()
 
 void binario(int decimal, Pilha &P)
 {
-    while( decimal > 0)
-    {   
+    while (decimal > 0)
+    {
         P.empilhar(decimal % 2);
         std::cout << "Valor empilhado: " << decimal % 2 << std::endl;
         decimal /= 2;
     }
 }
 
-int dec(Pilha &P)
+int Pilha::ConverDec()
 {
+    Nodo *aux;
+    aux = topo;
+    int contador = 0, decimal=0;
 
-
+    while (aux != nullptr)
+    {
+        contador++;
+        aux = aux->prox;
+    }
+    aux = topo;
+    contador--; // pois deve começar no zero
+    while (aux != nullptr)
+    {
+        if (aux->info == 0)
+        {
+            std::cout << "Numero 0"<< std::endl;
+            contador--;
+        }
+        else
+        {
+            decimal += pow(2, contador);
+            contador--;
+        }
+        aux = aux->prox;
+    }
+    std::cout << "Contador: " << contador << std::endl;
+    return decimal;
 }
 
-int main() 
-{   
-    int decimal = 0;
+int main()
+{
+    int d = 0;
     Pilha P;
 
     std::cout << "Digite um numero decimal: ";
-    std::cin >> decimal;
-    binario(decimal, P);
-    
-    int d = dec(P);
-    std::cout << "Numero em Binário: "; P.imprimir();
-    
+    std::cin >> d;
+    binario(d, P);
+
+    int conver = P.ConverDec();
+    std::cout << "Numero em Binário: ";
+    P.imprimir();
+    std::cout << "Numero em Decimal novamente: " << conver;    
     return 0;
 }
